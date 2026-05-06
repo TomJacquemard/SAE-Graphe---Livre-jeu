46f734b58450f04e196bd4b3c3f3c87126e0b34f
Laura988664 <laura.evrat@etu.univ-orleans.fr>
2026-05-06 12:02:06 +0200

    remplissage de pageJeu 2

--- src/PageJeu.java
+++ src/PageJeu.java
@@ -4,10 +4,32 @@ import java.util.ArrayList;
 public class PageJeu extends Livre{
     private boolean sortie;
     private List<PageJeu> pageSuivantes;
+    private ObjetJeu objet;
+    private List<Enigme> enigmes;
 
     public PageJeu(boolean sortie, ObjetJeu objet){
         this.sortie = sortie;
-        
+        this.objet = objet;
     }
 
+    public List<Enigme> getEnigmes(){
+        return enigmes;
+    }
+
+    public ObjetJeu getObjet(){
+        return objet;
+    }
+
+    public boolean estSortie(){
+        return sortie;
+    }
+
+    public boolean contientObjet(){
+        if (this.objet != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
