c8336ed1740eb155c028e02fdbce5c6d04e058c1
ErwanBeauhaire <erwan.beauhaire@etu.univ-orleans.fr>
2026-05-06 12:34:26 +0200

    modif LivreJeu ObjetJeu

--- src/LivreJeu.java
+++ src/LivreJeu.java
@@ -6,7 +6,8 @@ public class LivreJeu extends Livre{
     private List<ObjetJeu> lesObjets;
     private List<ObjetJeu> objetsRecuperes;
 
-    public LivreJeu(List<ObjetJeu> lesObjets, List<ObjetJeu> objetRecuperes) {
+    public LivreJeu(String titre, int nbPages) {
+        super(titre, nbPages);
         this.lesObjets = new ArrayList<>();
         this.objetsRecuperes = new ArrayList<>();
     }
@@ -16,18 +17,14 @@ public class LivreJeu extends Livre{
     }
 
     public List<ObjetJeu> getObjetsRecuperes() {
-        for (ObjetJeu o : lesObjets) {
-            if (o.estRecupere) {
-                objetsRecuperes.add(o);
-            }
-        }
-        return objetsRecuperes;
+        return this.objetsRecuperes;
     }
 
      // AJoute l'objet dans la liste des objets récupérés. 
 
     public void recupererObjet(ObjetJeu objet) { 
-        if (objet.recupererObjet()) {
+        objet.recupererObjet();
+        if (objet.estRecupere()){
             objetsRecuperes.add(objet);
         }
     }
