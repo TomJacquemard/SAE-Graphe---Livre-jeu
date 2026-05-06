47c5df545c8b12448a4dfa935a9c6a0a1f1baec6
GracaMicheleAngeleLimaMaiSango <graca-michele-angele.lima-mai-sango@etu.univ-orleans.fr>
2026-05-06 12:27:48 +0200

    Implémentation de la classe LivreJeu

--- src/LivreJeu.java
+++ src/LivreJeu.java
@@ -1,3 +1,34 @@
-public class LivreJeu{
-    
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class LivreJeu extends Livre{
+    private List<ObjetJeu> lesObjets;
+    private List<ObjetJeu> objetsRecuperes;
+
+    public LivreJeu(List<ObjetJeu> lesObjets, List<ObjetJeu> objetRecuperes) {
+        this.lesObjets = new ArrayList<>();
+        this.objetsRecuperes = new ArrayList<>();
+    }
+
+    public List<ObjetJeu> getListeObjets() {
+        return this.lesObjets;
+    }
+
+    public List<ObjetJeu> getObjetsRecuperes() {
+        for (ObjetJeu o : lesObjets) {
+            if (o.estRecupere) {
+                objetsRecuperes.add(o);
+            }
+        }
+        return objetsRecuperes;
+    }
+
+     // AJoute l'objet dans la liste des objets récupérés. 
+
+    public void recupererObjet(ObjetJeu objet) { 
+        if (objet.recupererObjet()) {
+            objetsRecuperes.add(objet);
+        }
+    }
 }
