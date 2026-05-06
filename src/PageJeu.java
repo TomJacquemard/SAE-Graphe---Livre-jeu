c3416898cb96682aee2534d3e5801ac8eb24015d
Laura988664 <laura.evrat@etu.univ-orleans.fr>
2026-05-06 11:45:28 +0200

    Remplissage de PageJeu

--- src/PageJeu.java
+++ src/PageJeu.java
@@ -1,4 +1,13 @@
-public class PageJeu extends Livre{
+import java.util.List;
+import java.util.ArrayList;
 
+public class PageJeu extends Livre{
+    private boolean sortie;
+    private List<PageJeu> pageSuivantes;
+
+    public PageJeu(boolean sortie, ObjetJeu objet){
+        this.sortie = sortie;
+        
+    }
 
 }
