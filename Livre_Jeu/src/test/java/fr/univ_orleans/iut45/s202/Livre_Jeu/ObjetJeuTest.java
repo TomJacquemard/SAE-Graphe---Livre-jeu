12376536eee0908a98ebbf4290d9ec85a24003ef
GracaMicheleAngeleLimaMaiSango <graca-michele-angele.lima-mai-sango@etu.univ-orleans.fr>
2026-05-13 12:22:18 +0200

    Création de tests pour les classes ObjetJeu et Page

---
+++ Livre_Jeu/src/test/java/fr/univ_orleans/iut45/s202/Livre_Jeu/ObjetJeuTest.java
@@ -0,0 +1,25 @@
+package fr.univ_orleans.iut45.s202.Livre_Jeu;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ObjetJeuTest {
+    ObjetJeu objet = new ObjetJeu("Ballon",false);
+
+    @Test
+    public void testGetNom() {
+        assertEquals("Ballon",objet.getNom());
+    }
+
+    @Test 
+    public void testEstRecupere() {
+        assertEquals(false, objet.estRecupere());
+    }
+
+    @Test 
+    public void testRecupereObjet() {
+        assertEquals(true, objet.recupererObjet());
+    }
+
+
+}
