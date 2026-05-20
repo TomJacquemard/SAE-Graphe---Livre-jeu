2056efb404525fe129694fa99378194b850f04c2
o22506351 <thomas.pajot@etu.univ-orleans.fr>
2026-05-20 12:07:35 +0200

    ajout classe testLivreJeu

---
+++ Livre_Jeu/src/test/java/fr/univ_orleans/iut45/s202/Livre_Jeu/TestLivreJeu.java
@@ -0,0 +1,31 @@
+package fr.univ_orleans.iut45.s202.Livre_Jeu;
+
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+
+public class TestLivreJeu {
+     List<ObjetJeu> lesObjets = new ArrayList<>();
+     List<ObjetJeu> objetsRecuperes = new ArrayList<>();
+    
+   
+    
+    LivreJeu lj = new LivreJeu("titre", 3);
+
+    @Test
+    public void getListObjects(){
+        assertEquals(lesObjets, lj.getListeObjets());
+    }
+
+    @Test
+    public void getObjetsRecuperes(){
+        assertEquals(lesObjets, lj.getObjetsRecuperes());
+    }
+
+    @Test 
+    public void recupererObjet(ObjetJeu object){
+        assertEquals(objetsRecuperes, object.estRecupere());
+    }
+    }
