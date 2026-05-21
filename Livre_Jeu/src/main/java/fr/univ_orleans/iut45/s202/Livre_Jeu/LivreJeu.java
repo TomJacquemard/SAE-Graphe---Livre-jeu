8eb91aabc3b71ec33888f7cd55e34a58d3702081
Laura988664 <laura.evrat@etu.univ-orleans.fr>
2026-05-21 02:14:08 +0200

    algo 1 prototype a tester

--- Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/LivreJeu.java
+++ Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/LivreJeu.java
@@ -39,9 +39,10 @@ public class LivreJeu extends Livre{
         Random random = new Random();
 
         while (!(pagesAPlacer.size()==0)){ //tant qu'il reste des pages à placer
-            PageJeu pageEnPlacement = pagesAPlacer.remove(-1);
+            PageJeu pageEnPlacement = pagesAPlacer.remove(0);
             Collections.shuffle(pagesValides); //je mélange la liste à chaque fois pour plus de hasard sur les liens entre les arrêtes
             
+            //Prblm ici, une page pourrait être reliée à énormément d'autres si on tombe pile sur pagesValides
             int nbPagesChoisies = 1 + random.nextInt(pagesValides.size()); //choisira un nb de pages entre 1 et pagesValide.size() (+1 pour éviter d'obtenir 0)
             List<PageJeu> pagesChoisies = pagesValides.subList(0, nbPagesChoisies); //on sélectionne les pages vers lesquelles la pageEnPlacement va mener
 
@@ -50,18 +51,50 @@ public class LivreJeu extends Livre{
                 int dureeEngime = 1+random.nextInt(20); //Choix aléatoire de la durée de l'Enigme menant à la page voisine p
                 pageEnPlacement.ajouteEnigme(new Enigme("Lorem Ipsum", dureeEngime, dureeEngime)); //création et ajout de l'énigme en parallèle de l'ajout de la page
             }
+            pagesValides.add(pageEnPlacement);
             //A la fin de la boucle while, toutes les pages seront placées, sauf la page d'entrée
         }
+
         List<PageJeu> pagesIsolees = this.pagesSansSource();
         if (pagesIsolees.size()>0){ //si il y des pages isolées...
-            for (PageJeu pIsolee : pagesIsolees){
+            for (PageJeu pIsolee : pagesIsolees){ //on relie la page d'entrée à ces pages
                 pageEntree.ajoutePage(pIsolee);
                 int dureeEngime = 1+random.nextInt(20); //Choix aléatoire de la durée de l'Enigme 
                 pageEntree.ajouteEnigme(new Enigme("Lorem Ipsum", dureeEngime, dureeEngime)); //création et ajout de l'énigme
-            
+            }
+        }
+        else{ //sinon je relie la page d'entrée à quelques pages
+            int nbPagesChoisies = 1 + random.nextInt(4); //4 pages maximum
+            List<PageJeu> pagesChoisies = pagesValides.subList(0, nbPagesChoisies); 
+            for(PageJeu p : pagesChoisies){
+                pageEntree.ajoutePage(p); 
+                int dureeEngime = 1+random.nextInt(20); 
+                pageEntree.ajouteEnigme(new Enigme("Lorem Ipsum", dureeEngime, dureeEngime)); //création et ajout de l'énigme en parallèle de l'ajout de la page
             }
         }
 
+        //Enfin on rajoute quelques pages menant vers le début
+        int nbPagesChoisies = 1 + random.nextInt(4); 
+        List<PageJeu> pagesChoisies = pagesValides.subList(0, nbPagesChoisies); 
+        for(PageJeu p : pagesChoisies){
+            p.ajoutePage(pageEntree); 
+            int dureeEngime = 1+random.nextInt(20); 
+            p.ajouteEnigme(new Enigme("Lorem Ipsum", dureeEngime, dureeEngime));
+            }
+    }
+
+    public void remplirGraphe(){
+        //On rempli les vertex et edge selon la liste lesPagesDuJeu
+        for(PageJeu pageCourante : lesPagesDuJeu){
+            this.graph.addVertex(pageCourante);
+            List<PageJeu> pagesSuivantes = pageCourante.getPagesSuivantes();
+            List<Enigme> enigmes = pageCourante.getEnigmes();
+            for(int i = 0; i<pagesSuivantes.size(); i ++){
+                DefaultWeightedEdge nouvelleEdge = this.graph.addEdge(pageCourante, pagesSuivantes.get(i));
+                
+                this.graph.setEdgeWeight(nouvelleEdge,enigmes.get(i).getDuree()); //on attribut le poids a la nouvelle arrete
+            }
+        }
     }
 
     public List<PageJeu> pagesSansSource(){
