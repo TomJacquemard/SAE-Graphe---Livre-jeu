b295403b944528a9f40ff549b7d1807176822ff5
TomJacquemard <tom.jacquemard@etu.univ-orleans.fr>
2026-05-21 12:11:06 +0200

    Mise à jour enigme

--- Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/Enigme.java
+++ Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/Enigme.java
@@ -2,11 +2,17 @@ package fr.univ_orleans.iut45.s202.Livre_Jeu;
 
 public class Enigme {
     private String intitule;
+    private double duree;
     private int difficultee;
 
-    public Enigme(String intitule, int difficultee){
+    public Enigme(String intitule, double duree, int difficultee){
         this.intitule = intitule;
+        this.duree = duree;
         this.difficultee = difficultee;
+    }
+
+    public double getDuree(){
+        return this.duree;
     }
 
     public int getDifficultee(){
