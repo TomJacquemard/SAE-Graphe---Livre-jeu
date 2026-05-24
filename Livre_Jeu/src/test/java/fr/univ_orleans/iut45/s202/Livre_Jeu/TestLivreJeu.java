8c1de9846b5e0c48e9a771bbdb1d3059a965e82b
Laura988664 <laura.evrat@etu.univ-orleans.fr>
2026-05-24 22:45:31 +0200

    test de genererLivreJeu_1 passe pas

--- Livre_Jeu/src/test/java/fr/univ_orleans/iut45/s202/Livre_Jeu/TestLivreJeu.java
+++ Livre_Jeu/src/test/java/fr/univ_orleans/iut45/s202/Livre_Jeu/TestLivreJeu.java
@@ -1,7 +1,14 @@
 package fr.univ_orleans.iut45.s202.Livre_Jeu;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import static org.junit.Assert.assertEquals;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.nio.AttributeType;
+import org.jgrapht.nio.DefaultAttribute;
+import org.jgrapht.nio.dot.DOTExporter;
 import org.junit.Test;
 
 
@@ -12,25 +19,33 @@ public class TestLivreJeu {
     List<PageJeu> pages = new ArrayList<>();
    
     
-    LivreJeu lj = new LivreJeu("titre", 3);
+    LivreJeu lj = new LivreJeu("titre", 15, "genererLivreJeu_1");
 
     @Test
-    public void getListObjects(){
-        assertEquals(lesObjets, lj.getListeObjets());
-    }
+    public void generationLivreJeu1() throws IOException{
+         DOTExporter<PageJeu,DefaultWeightedEdge> exporter = new DOTExporter<>();
+         exporter.setVertexAttributeProvider((x) -> Map.of("label", new DefaultAttribute<>(x, AttributeType.STRING)));
+		 exporter.exportGraph(lj.getGraphe(), new FileWriter("graph.dot"));
 
-    @Test
-    public void getObjetsRecuperes(){
-        assertEquals(objetsRecuperes, lj.getObjetsRecuperes());
     }
+    
+    // @Test
+    // public void getListObjects(){
+    //     assertEquals(lesObjets, lj.getListeObjets());
+    // }
 
-    @Test 
-    public void recupererObjet(ObjetJeu object){
-        assertEquals(objetsRecuperes, object.estRecupere());
-    }
+    // @Test
+    // public void getObjetsRecuperes(){
+    //     assertEquals(objetsRecuperes, lj.getObjetsRecuperes());
+    // }
 
-    @Test
-    public void getListePageJeu(){
-        assertEquals(pages, lj.getListePageJeu());
-    }
+    // @Test 
+    // public void recupererObjet(ObjetJeu object){
+    //     assertEquals(objetsRecuperes, object.estRecupere());
+    // }
+
+    // @Test
+    // public void getListePageJeu(){
+    //     assertEquals(pages, lj.getListePageJeu());
+    // }
     }
