5a23b9a6fa6e872899427b3d98456852d77d6003
Laura988664 <laura.evrat@etu.univ-orleans.fr>
2026-05-24 22:10:16 +0200

    suppression des imports inutiles

--- Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/Executable.java
+++ Livre_Jeu/src/main/java/fr/univ_orleans/iut45/s202/Livre_Jeu/Executable.java
@@ -1,24 +1,17 @@
-import java.io.File;
+package fr.univ_orleans.iut45.s202.Livre_Jeu;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.DefaultUndirectedGraph;
 import org.jgrapht.nio.AttributeType;
 import org.jgrapht.nio.DefaultAttribute;
-import org.jgrapht.nio.csv.CSVFormat;
-import org.jgrapht.nio.csv.CSVImporter;
 import org.jgrapht.nio.dot.DOTExporter;
-import org.jgrapht.util.SupplierUtil;
 
 
 public class Executable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
          LivreJeu livre = new LivreJeu("Livre Jeu test", 15,"genererLivreJeu_1");
 
          DOTExporter<PageJeu,DefaultWeightedEdge> exporter = new DOTExporter<>();
